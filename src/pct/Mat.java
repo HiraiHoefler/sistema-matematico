452ae0e5482834e2639a8ded5b4f08a7c901d08b
B1-08 <B1-08@B1-08-PC>
2021-11-12 20:35:46 -0200

    12/11

--- src/pct/Mat.java
+++ src/pct/Mat.java
@@ -54,8 +54,47 @@ public class Mat {
                     System.out.printf("%d! = %f", n, fat);                                                            
                     break;
                 case 2:
+                    //Declaração de variáveis
+                    //int x1, x2;
+                    int[] x; // = new int[4];
+                    double soma = 0, media;
+                    
+                    //opção
+                    System.out.println("informe a quantidade de números:");
+                    int num = entrada.nextInt();
+                    
+                    //construção do vetor
+                    x = new int[num];
+
                     //Média
                     System.out.println("\n\t\t\t Média \n");
+                    
+                    //Entradas
+                    for(int i = 0; i <= x.length; i++){
+                        System.out.print("Informe x1: ");
+                        x[i] = entrada.nextInt();
+                    }
+                    /*
+                    x[0] = entrada.nextInt();
+                    System.out.print("Informe x2: ");
+                    x[1] = entrada.nextInt();
+                    System.out.print("Informe x3: ");
+                    x[2] = entrada.nextInt();
+                    System.out.print("Informe x4: ");
+                    x[3] = entrada.nextInt();
+                    */
+
+                    //Processamento
+                    for(int m: x)
+                        soma += m;
+                      //soma += x[0] + x[1] + x[2] + x[3];
+                    media = soma/x.length;
+                    
+                    //Saída
+                    System.out.printf("\n Média de "); // %d e %d e %d e %d é %.2f.\n\n\n", x[0], x[1], x[2], x[3], media);
+                          for(int m: x)
+                            System.out.printf(" %d",m);
+                                System.out.printf("%.2.f \n\n\n ",media);
                     break;
                 case 3:
                     //Sair
